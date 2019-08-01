2f0c422cbd7fe287e059237d85b327dc1ba22964
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2019-08-01 16:39:53 +1000

    Updating to Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,169 +1,97 @@
-var gulp               = require('gulp');
-var del                = require('del');
-var gulpLoadPlugins    = require('gulp-load-plugins');
-var path               = require('path');
-var cleanCSS           = require('gulp-clean-css');
-var runSequence        = require('run-sequence');
-var del                = require('del');
-var fs                 = require('fs');
-var minify             = require('gulp-minify');
-var rename             = require('gulp-rename');
-var autoprefixer       = require('gulp-autoprefixer');
-var concat             = require('gulp-concat');
+const gulp        = require('gulp');
+const del         = require('del');
+const sass        = require('gulp-sass');
+const sourcemaps  = require('gulp-sourcemaps');
+const rename      = require("gulp-rename");
+const concat      = require("gulp-concat");
+const minify      = require('gulp-minify');
+const cleanCss    = require('gulp-clean-css');
+sass.compiler = require('node-sass');
 
-
-var options = {};
-
-options.rootPath = {
-  project     : __dirname + '/',
-  theme       : __dirname + '/'
+const options = {
+  paths : {
+    sass: 'sass/',
+    js: 'js/',
+    dist: 'dist/'
+  },
+  name: 'hds'
 };
 
-options.theme = {
-  root  : options.rootPath.theme,
-  sass  : options.rootPath.theme + 'sass/',
-  js    : options.rootPath.theme + 'js/',
-  dist  : options.rootPath.theme + 'dist/'
-};
+// ===================================
+// CSS
+// ===================================
 
-var sassFiles = [
-  options.theme.sass + '**/*.scss',
-  // Do not open Sass partials as they will be included as needed.
-  '!' + options.theme.sass + '**/_*.scss',
-  // Hide additional files
-  '!' + options.theme.sass + 'vendors/govau-ds.scss',
-  '!' + options.theme.sass + 'vendors/font-awesome/font-awesome.scss'
-];
-
-var $ = gulpLoadPlugins();
-/**
- * Generate styles with soucemap and uncompressed.
- */
-gulp.task('styles:dev', function(callback) {
-  runSequence(
-    'styles:clean',
-    'styles:generate:dev',
-    callback);
-});
-
-/**
- * Generate styles without sourcemap and compress them.
- */
-gulp.task('styles:prod', function(callback) {
-  runSequence(
-    'styles:clean',
-    'styles:generate:prod',
-    'styles:compress',
-    callback);
-});
-
-/**
- * Generate styles without sourcemap and compress them.
- */
-gulp.task('js:dev', function(callback) {
-  runSequence(
-    'js:clean',
-    'js:compress:dev',
-    callback);
-});
-
-/**
- * Generate styles without sourcemap and compress them.
- */
-gulp.task('js:prod', function(callback) {
-  runSequence(
-    'js:clean',
-    'js:compress:prod',
-    callback);
-});
-
-// Clean CSS files.
-gulp.task('styles:clean', function() {
+function cssClean() {
   return del([
-    options.theme.dist + '**/*.css',
-    options.theme.dist + '**/*.map',
-    '!' + options.theme.dist + 'print.css'
-  ], {force: true});
-});
+    options.paths.dist + '*.css',
+    options.paths.dist + 'maps'
+  ]);
+}
 
-/**
- * Compress the css.
- */
-gulp.task('styles:compress', function(){
-  return gulp.src(options.theme.dist + '/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest(options.theme.dist));
-});
-
-gulp.task('js:clean', function() {
-  return del([
-    options.theme.dist + '/*.js',
-  ], {force: true});
-});
-
-gulp.task('js:compress:dev', function() {
-  gulp.src(options.theme.js + 'src/*.js')
+function cssDev() {
+  return gulp.src(options.paths.sass + '*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
     .pipe(rename(function (path) {
-      path.basename += ".min";
-      path.extname = ".js";
-    }))
-    .pipe(gulp.dest(options.theme.dist))
-});
-
-gulp.task('js:compress:prod', function() {
-  gulp.src([options.theme.js + '/src/*.js'])
-    .pipe(minify({
-      ext: {
-        min:'.min.js'
-      },
-      noSource: true,
-    }))
-    .pipe(concat('healthgovau-ds.min.js'))
-    .pipe(gulp.dest(options.theme.dist))
-});
-
-/**
- * Generate css with source map.
- */
-gulp.task('styles:generate:dev', function(){
-  return gulp.src(sassFiles)
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      sourcemap: true,
-      precision: 10
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer())
-    .pipe($.sourcemaps.write('./'))
-    .pipe($.size({title: 'Styles'}))
-    .pipe(gulp.dest(options.theme.dist));
-});
-
-/**
- * Generate css without sourcemap.
- */
-gulp.task('styles:generate:prod', function(){
-  return gulp.src(sassFiles)
-    .pipe($.sass({
-      precision: 10,
-      sourcemap: false
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer())
-    .pipe($.size({title: 'Styles (production)'}))
-    .pipe(rename(function (path) {
-      path.basename += ".min";
+      path.basename = options.name + '-' + path.basename;
       path.extname = ".css";
     }))
-    .pipe(gulp.dest(options.theme.dist));
-});
+    .pipe(gulp.dest(options.paths.dist));
+}
 
-// Watch files for changes & reload
-gulp.task('watch', function(){
-  gulp.watch([options.theme.sass + '/**/*.scss']);
-  gulp.watch([options.theme.js + '/src/*.js']);
-});
+function cssProd() {
+  return gulp.src(options.paths.sass + '*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(cleanCss())
+    .pipe(rename(function (path) {
+      path.basename = options.name + '-' + path.basename;
+      path.extname = ".min.css";
+    }))
+    .pipe(gulp.dest(options.paths.dist));
+}
 
-// Watch and reload
-gulp.task('prod', ['styles:prod', 'js:prod']);
 
-// Watch and reload
-gulp.task('default', ['styles:dev', 'js:dev']);
+// ===================================
+// Javascript
+// ===================================
+
+function jsClean() {
+  return del([
+    options.paths.dist + '*.js'
+  ]);
+}
+
+function js() {
+  return gulp.src(options.paths.js + 'src/*.js')
+    .pipe(concat(options.name + '.js'))
+    .pipe(minify({
+      ext:{
+        min:'.min.js'
+      }
+    }))
+    .pipe(gulp.dest(options.paths.dist));
+}
+
+
+// ===================================
+// Watch
+// ===================================
+
+function watching() {
+  gulp.watch([options.paths.js + 'src/*.js'], gulp.series(jsClean, js));
+  gulp.watch([options.paths.sass + '**/*.scss'], gulp.series(cssClean, gulp.parallel(cssDev, cssProd)));
+}
+
+// ===================================
+// Exports
+// ===================================
+
+exports.default = gulp.parallel(
+  gulp.series(jsClean, js),
+  gulp.series(cssClean, gulp.parallel(cssDev, cssProd))
+);
+
+exports.watch = watching;
