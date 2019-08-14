4ac85b5ea9979c8bdcc19123a3e6e7e59f7a8f53
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2019-08-14 17:17:56 +1000

    Refactoring drupal specific styles and javascript

---
+++ source/js/src/health.accordion.js
@@ -0,0 +1,5 @@
+(function ($) {
+  $(document).ready(function () {
+    $('html').removeClass('no-js').addClass('js');
+  });
+})(jQuery);
--- source/js/src/health.search.js
+++ source/js/src/health.search.js
@@ -1,99 +1,116 @@
+/**
+ * Add an autocomplete dropdown to a text input.
+ *
+ * @param selector
+ *   jQuery selector.
+ * @param source
+ *   Callback function or array of terms.
+ *
+ * @see https://api.jqueryui.com/autocomplete/
+ */
+function healthSearchAutocomplete(selector, source) {
+  $(selector).once('healthSearchAutocomplete').autocomplete({
+    source: source,
+    minLength: 2,
+    classes: {
+      "ui-autocomplete": "au-body au-link-list"
+    },
+    // When the user selects a value, submit the form.
+    select: function( event, ui ) {
+      $(this).val(ui.item.value);
+      $(event.target).parents('form').submit();
+    },
+    // Show loading spinner.
+    search: function( event, ui ) {
+      $(event.target).parents('form').find('.health-loading').toggleClass('health-loading--active');
+    },
+    // Hide loading spinner.
+    response: function( event, ui ) {
+      $(event.target).parents('form').find('.health-loading').toggleClass('health-loading--active');
+    }
+  });
+}
+
+/**
+ * Move the search box depending on if we are looking at mobile or desktop.
+ */
+function healthSearchResize() {
+  if ($(window).width() > 769) {
+    if ($('.au-main-nav .health-search--global').length !== 0) {
+      $('.health-search--global').insertAfter('.health-sub-nav').show();
+    }
+    $('.health-facet').removeClass('health-facet--mobile-hidden');
+  } else {
+    if ($('.au-main-nav .health-search--global').length === 0) {
+      $('.health-search--global').insertAfter('#main-nav-default').hide();
+    }
+    $('.health-facet').addClass('health-facet--mobile-hidden');
+  }
+}
+
+/**
+ * Limit the number of options displayed in a facet group by providing a show more and show less link.
+ *
+ * @param selector
+ * @param limit
+ */
+function healthFacetShowMore(selector, limit) {
+  $(selector).once('healthFacetShowMore').each(function() {
+    var
+      limit_css = limit - 1,
+      facet = $(this);
+
+    // Hide filters beyond the limit.
+    facet.find('.au-control-input:gt(' + limit_css + ')').hide();
+
+    // Add a 'Show more' button if the facet has more than the limit.
+    facet.filter(function() {
+      return facet.find('.au-control-input').length > limit;
+    }).each(function() {
+      $('<button aria-controls="' + facet.attr('id') + '" class="health-facet__more au-btn au-btn--tertiary">').text('Show more').click(function () {
+        if (facet.find('.au-control-input:hidden').length > 0) {
+          facet.find('.au-control-input:gt(' + limit_css + ')').show();
+          $(this).addClass('open').text('Show less');
+        }
+        else {
+          facet.find('.au-control-input:gt(' + limit_css + ')').hide();
+          $(this).removeClass('open').text('Show more');
+        }
+        return false;
+      }).insertAfter(facet.find('.health-facet__list'));
+    });
+  });
+}
+
 (function ($) {
   $(document).ready(function () {
-    // Autocomplete.
-    $("#edit-funnelback-search-field, #edit-funnelback-search-field--2").autocomplete({
-      source: function( request, response ) {
-        // Get results from funnelback.
-        $.ajax({
-          url: '/funnelback/search/autocompletion/' + request.term,
-          dataType: "json",
-          success: function( data ) {
-            response( data );
-          }
-        });
-      },
-      minLength: 2,
-      classes: {
-        "ui-autocomplete": "au-body au-link-list"
-      },
-      // When the user selects a value, submit the form.
-      select: function( event, ui ) {
-        $(this).val(ui.item.value);
-        $(event.target).parents('form').submit();
-      },
-      // Show loading spinner.
-      search: function( event, ui ) {
-        $(event.target).parents('form').find('.health-loading').toggleClass('health-loading--active');
-      },
-      // Hide loading spinner.
-      response: function( event, ui ) {
-        $(event.target).parents('form').find('.health-loading').toggleClass('health-loading--active');
-      }
-    });
-
-    // Resizing.
-    function resizeFunction() {
-      if ($(window).width() > 769) {
-        if ($('.au-main-nav .health-search--global').length !== 0) {
-          $('.health-search--global').insertAfter('#block-menu-menu-sub-menu').show();
-        }
-      } else {
-        if ($('.au-main-nav .health-search--global').length === 0) {
-          $('.health-search--global').insertAfter('#main-nav-default').hide();
-          $('.au-main-nav__toggle--search i').addClass('fa-search').removeClass('fa-times');
-        }
-      }
-    }
 
     var resizeTimer; // Set resizeTimer to empty so it resets on page load
     // On resize, run the function and reset the timeout
     $(window).resize(function() {
       clearTimeout(resizeTimer);
-      resizeTimer = setTimeout(resizeFunction, 250);
+      resizeTimer = setTimeout(healthSearchResize, 250);
     });
-    resizeFunction();
+    healthSearchResize();
+
+    // Copy across the elements from the sub menu into the main nav.
+    // Only show them in the mobile menu.
+    $('.health-sub-nav li').clone().insertAfter('.au-main-nav ul li:last-of-type').addClass('au-main-nav--mobile-only');
 
     // Search button handler.
     $('.au-main-nav__toggle--search').click(function(e) {
       $('.health-search--global').toggle();
-      $(this).find('i').toggleClass('fa-search').toggleClass('fa-times');
+      $(this).find('span.svg-inline--fa').toggle();
     });
 
     // Filter toggles.
     $('.health-filter__title').click(function (e) {
-      $(this).toggleClass('expanded');
-      $('.block-facetapi, .block-funnelback').toggleClass('facetshow');
+      $('.health-facet').toggleClass('health-facet--mobile-hidden');
+      $(this).toggleClass('health-filter--open');
     });
 
-    // Add correct grid class to seccond refine search in listings.
+    // Add correct grid class to second refine search in listings.
     $('.region-navigation #search-api-page-search-form-default-search--2').addClass('col-xs-12');
 
-    // Add 'Show more' to facets.
-    $('.health-facets').each(function() {
-      var
-        limit = 8,
-        limit_css = limit - 1,
-        facet = $(this);
-
-      // Hide filters beyond the limit.
-      facet.find('li:gt(' + limit_css + ')').hide();
-
-      // Add a 'Show more' button if the facet has more than the limit.
-      facet.filter(function() {
-        return facet.find('li').length > limit;
-      }).each(function() {
-        $('<button aria-controls="' + facet.attr('id') + '" class="health-filter__more au-btn au-btn--tertiary">').text('Show more').click(function () {
-          if (facet.find('li:hidden').length > 0) {
-            facet.find('li:gt(' + limit_css + ')').show();
-            $(this).addClass('open').text('Show less');
-          }
-          else {
-            facet.find('li:gt(' + limit_css + ')').hide();
-            $(this).removeClass('open').text('Show more');
-          }
-          return false;
-        }).insertAfter(facet);
-      });
-    });
   });
 })(jQuery);
