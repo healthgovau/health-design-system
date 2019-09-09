2e016fbbd6a6417da035e99a68e0b003a8ec058a
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2019-09-09 12:17:06 +1000

    Style guide: Added tooltip

--- style-guide/public/build/js/hds.js
+++ style-guide/public/build/js/hds.js
@@ -213,6 +213,31 @@ var health = health || {};
 
 })(jQuery);
 
+var health = health || {};
+
+(function ($) {
+  $(document).ready(function () {
+
+    health.tooltip = function(selector, options) {
+      if (typeof tippy !== 'undefined') {
+        var defaultOptions = {
+          arrow: false,
+          theme: 'health-tooltip',
+          performance: true,
+          allowTitleHTML: true,
+          interactive: true,
+          animateFill: false,
+        };
+        options = options || {};
+        options = $.extend(options, defaultOptions);
+        return tippy(selector, options);
+      } else {
+        console.log('Tooltip library has not been loaded.');
+      }
+    }
+
+  });
+})(jQuery);
 (function ($) {
   $(document).ready(function () {
     //YouTube embed on click
