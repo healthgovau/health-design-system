7ebcd2f10104015892adcec606508c52967409b9
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2019-09-18 16:37:53 +1000

    Build

--- build/js/hds.js
+++ build/js/hds.js
@@ -44,6 +44,65 @@
   });
 })(jQuery);
 
+var health = health || {};
+
+(function ($) {
+  /**
+   * Add a dynamic character counter to the bottom of a input field.
+   *
+   * @param selector
+   *   jQuery selector for an input or textarea.
+   *
+   * @param maxlength
+   *   How many characters to restrict. If ommited, will use maxlength attribute if found.
+   */
+  health.fieldLengthCounter = function (selector, maxlength) {
+
+    $(selector).each(function() {
+
+      var $counter = $('<span class="text--minor"></span>');
+      var maxlength = parseInt(maxlength) || parseInt($(this).attr('maxlength')) || 0;
+
+      $(this).after($counter);
+
+      $(this).keyup(function () {
+        var remaining = maxlength - $(this).val().length;
+        if (remaining < 0) {
+          remaining = 0;
+        }
+        $counter.text(remaining + ' characters remaining');
+      });
+
+      $(this).trigger('keyup');
+    });
+  };
+})(jQuery);
+var health = health || {};
+
+(function ($) {
+
+  /**
+   * Dynamically generate the in page nav links.
+   *
+   * Will only look for and link to H2's.
+   *
+   * @param selector
+   *   The area to look for the links, in a jQuery selector.
+   */
+  health.inpagenav = function(selector) {
+    if (typeof $.fn.anchorific !== 'undefined') {
+      $(selector).anchorific({
+        navigation: '.au-inpage-nav-links',
+        headers: 'h2',
+        anchorText: false,
+        spy: false
+      });
+    }
+  };
+
+})(jQuery);
+
+
 (function ($) {
   $(document).ready(function () {
     if (typeof LazyLoad !== 'undefined') {
