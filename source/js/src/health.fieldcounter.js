f9fe9000cb414601034fbff15322bd96f0ab8288
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2019-09-18 15:56:46 +1000

    Field counter: Fixed incorrect scoping of function

--- source/js/src/health.fieldcounter.js
+++ source/js/src/health.fieldcounter.js
@@ -1,33 +1,33 @@
 var health = health || {};
 
 (function ($) {
-  $(document).ready(function () {
+  /**
+   * Add a dynamic character counter to the bottom of a input field.
+   *
+   * @param selector
+   *   jQuery selector for an input or textarea.
+   *
+   * @param maxlength
+   *   How many characters to restrict. If ommited, will use maxlength attribute if found.
+   */
+  health.fieldLengthCounter = function (selector, maxlength) {
 
-    /**
-     * Add a dynamic character counter to the bottom of a input field.
-     *
-     * @param selector
-     *   jQuery selector for an input or textarea.
-     *
-     * @param maxlength
-     *   How many characters to restrict. If ommited, will use maxlength attribute if found.
-     */
-    health.fieldLengthCounter = function (selector, maxlength) {
-      var $field = $(selector);
+    $(selector).each(function() {
+
       var $counter = $('<span class="text--minor"></span>');
-      var maxlength = parseInt(maxlength) || parseInt($field.attr('maxlength')) || 0;
+      var maxlength = parseInt(maxlength) || parseInt($(this).attr('maxlength')) || 0;
 
-      $field.after($counter);
+      $(this).after($counter);
 
-      $field.keyup(function() {
-        var remaining = maxlength - $field.val().length;
+      $(this).keyup(function () {
+        var remaining = maxlength - $(this).val().length;
         if (remaining < 0) {
           remaining = 0;
         }
         $counter.text(remaining + ' characters remaining');
       });
 
-      $field.trigger('keyup');
-    };
-  });
+      $(this).trigger('keyup');
+    });
+  };
 })(jQuery);
