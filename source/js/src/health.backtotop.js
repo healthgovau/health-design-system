d81b447bc8decf670f52aff33d127aaa87a7f5e3
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2019-10-01 12:16:27 +1000

    Back to top: Fixed flickering of back to top after being clicked

--- source/js/src/health.backtotop.js
+++ source/js/src/health.backtotop.js
@@ -1,19 +1,27 @@
 (function ($) {
+
+  var ignore = false;
   $(document).ready(function () {
     var $backToTop = $('.health-back-to-top');
 
     $(document).bind('scroll', function () {
-      if ($(document).scrollTop() > 1000) {
-        $backToTop.addClass('isVisible');
-      } else {
-        $backToTop.removeClass('isVisible');
+      if (!ignore) {
+        if ($(document).scrollTop() > 1000) {
+          $backToTop.addClass('isVisible');
+        } else {
+          $backToTop.removeClass('isVisible');
+        }
       }
     });
 
     $backToTop.click(function() {
+      ignore = true;
+      $backToTop.removeClass('isVisible');
       $('html,body').stop().animate({
-        scrollTop: 0
-      }, 'slow', 'swing');
+        scrollTop: 0,
+      }, 'slow', 'swing', function() {
+        ignore = false;
+      });
     });
   });
 })(jQuery);
