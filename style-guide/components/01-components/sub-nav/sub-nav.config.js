2086ee0d864ccf51a76367ecd48f306a1fdc5983
Ben Gunn <ben.gunn@health.gov.au>
2020-01-01 11:59:07 +1100

    Updated Sub-nav component to work with both Fractal and Drupal

---
+++ style-guide/components/01-components/sub-nav/sub-nav.config.js
@@ -0,0 +1,26 @@
+const config = {
+  title: "Sub Nav",
+  context: {
+    menu_name: "My sub menu",
+    items: [
+      {
+        href: "#",
+        text: "About us"
+      },
+      {
+        href: "#",
+        text: "Media centre"
+      },
+      {
+        href: "#",
+        text: "News"
+      },
+      {
+        href: "#",
+        text: "Contact us"
+      },
+    ]
+  }
+};
+
+module.exports = config;
