31576eb3c8f30e72401b65b6fdfdfc7a5ca5b22c
Ben Gunn <ben.gunn@health.gov.au>
2020-01-01 11:58:29 +1100

    Updated default Header component to work with both Fractal and Drupal

---
+++ style-guide/components/01-components/header/header.config.js
@@ -0,0 +1,28 @@
+const config = {
+  title: "Header",
+  preview: "@preview-full-width",
+  context: {
+    logo_src: "http://via.placeholder.com/300x73",
+    menu_name: "My sub menu",
+    items: [
+      {
+        href: "#",
+        text: "About us"
+      },
+      {
+        href: "#",
+        text: "Media centre"
+      },
+      {
+        href: "#",
+        text: "News"
+      },
+      {
+        href: "#",
+        text: "Contact us"
+      },
+    ]
+  }
+}
+
+module.exports = config;
