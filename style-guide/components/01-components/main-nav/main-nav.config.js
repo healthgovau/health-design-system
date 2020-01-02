1941ac08cda1c4476c29e771e62fadcae8b4e0f3
Ben Gunn <ben.gunn@health.gov.au>
2020-01-02 15:00:36 +1100

    Updated Main Navigation component to work with both Fractal and Drupal

---
+++ style-guide/components/01-components/main-nav/main-nav.config.js
@@ -0,0 +1,15 @@
+const config = {
+  title: "Main navigation",
+  label: "Main navigation",
+  preview: "@preview-full-width",
+  context: {
+    "title": "Search this website",
+    "button_text": {
+      "menu": "Menu",
+      "search": "Search",
+      "close": "Close"
+    }
+  }
+}
+
+module.exports = config
