459783a5a1f080a040b5b5772f7b4f9b15264dbd
Danny <dannymarlow78@hotmail.com>
2020-02-27 16:42:13 +1100

    NST dev

--- style-guide/fractal.js
+++ style-guide/fractal.js
@@ -37,7 +37,7 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
  * Tell the Fractal web preview plugin where to look for static assets.
  */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
-
+fractal.web.set('favicon', path.join(__dirname, 'favicon.ico'));
 /* Generated HTML export destination */
 fractal.web.set('builder.dest', __dirname + '/../docs');
 
