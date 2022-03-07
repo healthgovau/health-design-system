72e740c19ed24bc0174cbb323f7e93e024a8972c
Hugo <hugo.heuman@health.gov.au>
2022-03-07 08:58:16 +0800

    [GOLD] Added eslint to project, formatted code and added comments.

--- bin/setup.js
+++ bin/setup.js
@@ -1,69 +1,74 @@
 #! /usr/bin/env node
-const path = require('path');
+// const path = require('path');
 const fs = require('fs-extra');
-var getDeps = require('get-dependencies');
-const {getInstalledPathSync} = require('get-installed-path');
+const getDeps = require('get-dependencies');
+const { getInstalledPathSync } = require('get-installed-path');
 
-console.log("Creating Health Design System starter kit base.");
+console.log('Creating Health Design System starter kit base.');
 
-//Collect paths
-const gold = getInstalledPathSync('@gold.au', {local: true});
-const hds = getInstalledPathSync('@health.gov.au/health-design-system', {local: true});
-const base_path = process.cwd();
-const srcDir = hds + '/source';
-const destDir = process.cwd() + `/assets/hds`;
+// Collect paths
+const hds = getInstalledPathSync('@health.gov.au/health-design-system', { local: true });
+const basePath = process.cwd();
+const srcDir = `${hds}/source`;
+const destDir = `${process.cwd()}/assets/hds`;
 
-
-//Copy files to assets folder
+// Copy files to assets folder
 try {
-  fs.copySync(srcDir, destDir, {overwrite: true})
-  console.log("■ Files copied to assets folder");
+  fs.copySync(srcDir, destDir, { overwrite: true });
+  console.log('■ Files copied to assets folder');
 } catch (err) {
-  console.error(err)
+  console.error(err);
 }
 
-//Check if assets/scss/main.scss exists, if not create it with reference.
-if (!fs.existsSync(base_path + '/assets/scss')) {
-  fs.mkdirSync(base_path + '/assets/scss');
+// Check if assets/scss/main.scss exists, if not create it with reference.
+if (!fs.existsSync(`${basePath}/assets/scss`)) {
+  fs.mkdirSync(`${basePath}/assets/scss`);
 }
-if (fs.existsSync(base_path + '/assets/scss/main.scss')) {
-  console.log("/assets/scss/main.scss exists, ensure that it contains:\n  @import '../hds/sass/all.scss';\nto utilise HDS");
+if (fs.existsSync(`${basePath}/assets/scss/main.scss`)) {
+  console.log('/assets/scss/main.scss exists, ensure that it contains:\n  @import \'../hds/sass/all.scss\';\nto utilise HDS');
 } else {
-  fs.writeFile(base_path + '/assets/scss/main.scss', "@import '../hds/sass/all.scss';", function (err) {
+  fs.writeFile(`${basePath}/assets/scss/main.scss`, '@import \'../hds/sass/all.scss\';', (err) => {
     if (err) {
       return console.log(err);
     }
-    console.log("■ Created /assets/scss/main.scss and added import statement for HDS");
+    console.log('■ Created /assets/scss/main.scss and added import statement for HDS');
+    return true;
   });
 }
 
-//Add GOLD dependencies
-getDeps.getByFile(hds + '/package.json')
-  .then(function (result) {
-    let r = result.filter(val => val.startsWith('@gold') && !val.includes('pancake')).map(filt => '@import "' + base_path + '/node_modules/' + filt + '/lib/sass/_module.scss";');
+// Add GOLD dependencies
 
+// Get dependencies for the Health Design System.
+getDeps.getByFile(`${hds}/package.json`)
+  .then((result) => {
+    //  Only process dependencies that starts with @gold and not includes the word pancake.
+    //  Add correct path to the include statement.
+    const r = result.filter((val) => val.startsWith('@gold') && !val.includes('pancake'))
+      .map((filt) => `@import "${basePath}/node_modules/${filt}/lib/sass/_module.scss";`);
+
+    // Add comment to top of array.
     r.unshift('\n//GOLD SCSS dependencies. ');
 
-    fs.appendFile(process.cwd() + `/assets/hds/sass/all.scss`, r.join("\n"), function (err) {
+    // Append dependencies to all.scss
+    fs.appendFile(`${process.cwd()}/assets/hds/sass/all.scss`, r.join('\n'), (err) => {
       if (err) throw err;
       console.log('■ GOLD references written to scss file.');
     });
   });
 
-//Copy and rename pancake ref file
-if (!fs.existsSync(base_path + '/assets/hds/sass/vendors')) {
-  fs.mkdirSync(base_path + '/assets/hds/sass/vendors');
+// Copy and rename pancake ref file
+if (!fs.existsSync(`${basePath}/assets/hds/sass/vendors`)) {
+  fs.mkdirSync(`${basePath}/assets/hds/sass/vendors`);
 }
-if (fs.existsSync(base_path + '/pancake/sass/pancake.scss')) {
-  fs.copyFile(base_path + '/pancake/sass/pancake.scss', base_path + '/assets/hds/sass/vendors/govau-ds.scss', (err) => {
+if (fs.existsSync(`${basePath}/pancake/sass/pancake.scss`)) {
+  fs.copyFile(`${basePath}/pancake/sass/pancake.scss`, `${basePath}/assets/hds/sass/vendors/govau-ds.scss`, (err) => {
     if (err) throw err;
     console.log('■ Pancake reference file copied and renamed.');
   });
 } else {
-  console.log('\x1b[31m%s\x1b[0m','■ WARNING! No pancace folder found, cannot copy file. Delete the node_modules folder and try again.');
+  console.log('\x1b[31m%s\x1b[0m', '■ WARNING! No pancace folder found, cannot copy file. Delete the node_modules folder and try again.');
 }
 
-
-//Remove pancake folder
-fs.rmdirSync(base_path + '/pancake', {recursive: true});
-console.log("■ Removed pancake folder");
+// Remove pancake folder
+fs.rmdirSync(`${basePath}/pancake`, { recursive: true });
+console.log('■ Removed pancake folder');
