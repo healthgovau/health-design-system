9dfc1d61c4560e4513009ea224107b32e11b859a
Adrian <adrian.conte@technocrat.com.au>
2023-09-27 08:45:31 +1000

    440393 - Add --hero-text colour variable, rebuild assets.

--- build/js/modules/health.backtotop.js
+++ build/js/modules/health.backtotop.js
@@ -1,4 +1,4 @@
-/*! @health.gov.au/health-design-system v1.5.1 */
+/*! @health.gov.au/health-design-system v2.0.0-beta.1 */
 (function ($) {
 
   var ignore = false;
--- style-guide/public/build/js/modules/health.accordion.js
+++ style-guide/public/build/js/modules/health.accordion.js
@@ -1,4 +1,4 @@
-/*! @health.gov.au/health-design-system v1.5.1 */
+/*! @health.gov.au/health-design-system v2.0.0-beta.1 */
 (function ($) {
   $(document).ready(function () {
     $('html').removeClass('no-js').addClass('js');
