1c80dcdad86cb325e6e39470ad78c37a6db8e45e
Ben Gunn <ben.gunn@health.gov.au>
2025-02-19 17:36:44 +1100

    1507730 - Created PoC of tooltip using Floating UI library

--- style-guide/public/build/js/hds.js
+++ style-guide/public/build/js/hds.js
@@ -388,27 +388,51 @@ var health = health || {};
 
 var health = health || {};
 
-(function ($) {
-  $(document).ready(function () {
+(($, FloatingUIDOM, document) => {
+  $(document).ready(() => {
+    health.tooltip = (selector, options) => {
+      if (typeof FloatingUIDOM === 'object') {
+        const template = `
+          <div class="health-tooltip">
+            <div class="health-tooltip__content"><h2>Hi there!</h2></div>
+          </div>
+        `;
+        const $selector = $(selector);
+        const $tooltip = $(template);
+        const $tooltipContent = $tooltip.find('.health-tooltip__content');
 
-    health.tooltip = function(selector, options) {
-      if (typeof tippy !== 'undefined') {
-        var defaultOptions = {
-          arrow: false,
-          theme: 'health-tooltip',
-          performance: true,
-          allowTitleHTML: true,
-          interactive: true,
-          animateFill: false,
-        };
-        options = options || {};
-        options = $.extend(options, defaultOptions);
-        return tippy(selector, options);
+        // Process tooltip content.
+        const content = $selector.attr('title');
+        if (content) {
+          $selector
+            .attr('data-health-tooltip', content)
+            .removeAttr('title');
+        }
+
+        $selector
+          .on('focus mouseenter touchstart', (event) => {
+            // Set tooltip content.
+            $tooltipContent.html(content);
+            // Display tooltip.
+            $selector.append($tooltip);
+            // Set tooltip position.
+            FloatingUIDOM.computePosition($selector[0], $tooltip[0]).then(({ x, y }) => {
+              Object.assign($tooltip.css({
+                left: `${x}px`,
+                top: `${y}px`,
+              }));
+            });
+          })
+          .on('blur mouseleave touchend', () => {
+            $tooltip.remove();
+            $tooltipContent.html('');
+          });
       }
-    }
 
+      return null;
+    };
   });
-})(jQuery);
+})(jQuery, window.FloatingUIDOM, document);
 (function ($) {
   $(document).ready(function () {
     //YouTube embed on click
