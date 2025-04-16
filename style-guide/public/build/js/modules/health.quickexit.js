8fe29a7d13eea5aeaf50259f4a7c327a36bc7b00
Ben Gunn <ben.gunn@health.gov.au>
2025-04-16 15:02:13 +1000

    1507847 - Updated release version number

--- style-guide/public/build/js/modules/health.quickexit.js
+++ style-guide/public/build/js/modules/health.quickexit.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/*! @health.gov.au/health-design-system v3.1.1 */
+/*! @health.gov.au/health-design-system v3.1.2 */
 /*! @health.gov.au/health-design-system v2.0.0-beta.3 */
 var health = health || {};
 (function ($) {
