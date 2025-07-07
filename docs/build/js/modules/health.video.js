7c8cbddb6c8cd31dcfbd03ac779eb1cdc0f57ec6
Ben Gunn <ben.gunn@health.gov.au>
2025-07-08 09:49:24 +1000

    1405739 - Updated release version number

--- docs/build/js/modules/health.video.js
+++ docs/build/js/modules/health.video.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/*! @health.gov.au/health-design-system v3.1.4 */
+/*! @health.gov.au/health-design-system v3.1.5 */
 (function ($) {
   $(document).ready(function () {
     //YouTube embed on click
--- style-guide/public/build/js/modules/health.accordion.js
+++ style-guide/public/build/js/modules/health.accordion.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/*! @health.gov.au/health-design-system v3.1.4 */
+/*! @health.gov.au/health-design-system v3.1.5 */
 (function ($) {
   $(document).ready(function () {
     $('html').removeClass('no-js').addClass('js');
